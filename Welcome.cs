70583c35ca103b1820c63be335384427d5408b8c
Aurore Reynier <reynier.aurore@gmail.com>
2023-11-30 12:18:31 +0100

    réorganisation des CSV

--- Welcome.cs
+++ Welcome.cs
@@ -1,12 +1,9 @@
-using RetrieveCSVFiles;
-
 namespace Welcome
 
 {
     public static class WelcomeMessages {
         public static void PrintMessages() {
             Console.WriteLine("Bienvenue sur la plate-forme de quizz: que voulez-vous faire?");
-            RetrieveCSVFilesNumber.HaveCSVFilesNumber();
             Console.WriteLine("1. Démarrer un quizz au hasard");
             Console.WriteLine("2. Choisir une catégorie");
             Console.WriteLine("3. Quitter");
